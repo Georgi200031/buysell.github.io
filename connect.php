88641d15c213e96a42ce970a2c3c25b4ad069e2e
dev200031 <77017548+Georgi200031@users.noreply.github.com>
2021-09-17 01:24:26 +0300

    commit

--- connect.php
+++ connect.php
@@ -1,7 +1,42 @@
 <?php
-$dbhost = 'localhost';
-         $dbuser = 'root';
-         $dbpass = 'Georgi123';
-         $dbname = 'schema';
-         $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$connect = mysqli_connect("localhost", "root", "", "Georgi123");
+$sql = "SELECT * FROM products";
+$result = mysqli_query($connect, $sql);
 ?>
+<!DOCTYPE html>
+<html>
+     <head>
+          <title>Webslesson Tutorial | How to use Mysql View in PHP Code</title>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+     </head>
+     <body>
+          <br />
+          <div class="container" style="width:500px;">
+               <h3 align="">How to Use Mysql View in PHP Code</h3><br />
+               <div class="table-responsive">
+                    <table class="table table-striped">
+                         <tr>
+                              <th>Brand ID</th>
+                              <th>Brand Name</th>
+                              <th>Product Name</th>
+                         </tr>
+                         <?php
+                         while($row = mysqli_fetch_array($result))
+                         {
+                         ?>
+                         <tr>
+
+                              <td><?php echo $row["price"];?></td>
+
+                         </tr>
+                         <?php
+                         }
+                         ?>
+                    </table>
+               </div>
+          </div>
+          <br />
+     </body>
+</html>
